2b3fdcb89d7a5f3417f22e08914fd4176ea8da6f
AlexDocking <1998adocking@gmail.com>
2022-07-29 16:49:58 +0100

    Put red dot on image to show where the oil extraction site is

--- Better Oil/Tests/TestOilDepletion.cs
+++ Better Oil/Tests/TestOilDepletion.cs
@@ -3,6 +3,8 @@ using Moq;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -493,7 +495,40 @@ namespace BetterOil.Tests
                     }
                 }
                 cumulativeBarrels += barrelsToExtract;
-                image.Save(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Oil Heatmap " + TimeSpan.FromSeconds(cumulativeTime.LastOrDefault()).ToString(@"\Ddd\ \Hhh") + " After " + cumulativeBarrels + " Barrels.png"), System.Drawing.Imaging.ImageFormat.Png);
+                Bitmap resized = ResizeImage(image, image.Width * 5, image.Height * 5);
+                resized.SetPixel(2 + 5 * (pumpX - left), 2 + 5 * (pumpY - bottom), Color.Red);
+                resized.Save(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "Oil Heatmap " + TimeSpan.FromSeconds(cumulativeTime.LastOrDefault()).ToString(@"\Ddd\ \Hhh") + " After " + cumulativeBarrels + " Barrels.png"), System.Drawing.Imaging.ImageFormat.Png);
+            }
+            /// <summary>
+            /// Resize the image to the specified width and height. https://stackoverflow.com/questions/1922040/how-to-resize-an-image-c-sharp
+            /// </summary>
+            /// <param name="image">The image to resize.</param>
+            /// <param name="width">The width to resize to.</param>
+            /// <param name="height">The height to resize to.</param>
+            /// <returns>The resized image.</returns>
+            public static Bitmap ResizeImage(Image image, int width, int height)
+            {
+                var destRect = new Rectangle(0, 0, width, height);
+                var destImage = new Bitmap(width, height);
+
+                destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+                using (var graphics = Graphics.FromImage(destImage))
+                {
+                    graphics.CompositingMode = CompositingMode.SourceCopy;
+                    graphics.CompositingQuality = CompositingQuality.HighQuality;
+                    graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                    using (var wrapMode = new ImageAttributes())
+                    {
+                        wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                        graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                    }
+                }
+
+                return destImage;
             }
         }
     }
